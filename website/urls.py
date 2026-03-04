8d0dcd308cf202e5a8f1d18afc5182ae6ba84be2
Arbaz Khan <arbazkhanrbk@gmail.com>
2026-03-04 23:46:57 +0500

    first coomit on my new rental website

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('properties/', views.properties, name='properties'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('contact/', views.contact, name='contact'),
+]
