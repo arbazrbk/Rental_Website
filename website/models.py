d56e0a954daa797fbd8b1f4ff22128ce6b4704cf
Arbaz Khan <arbazkhanrbk@gmail.com>
2026-03-05 00:14:01 +0500

    added modules

--- website/models.py
+++ website/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    price = models.CharField(max_length=100)
+    property_type = models.CharField(max_length=50)
+    beds = models.IntegerField()
+    baths = models.IntegerField()
+    sqft = models.IntegerField()
+    image = models.ImageField(upload_to='properties/')
+    
+class Service(models.Model):
+    icon = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)    
+    description = models.TextField()
+    
+class feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    
+
+class register(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField()
+    country = models.CharField(max_length=100)
+    password = models.CharField(max_length=128) 
+ 
+ 
+            
